7567a5fa5eb21dc6686ae07d496d82cec2caf757
Madhuri Bhamidipati <mbhamidipati22@gmail.com>
2017-04-26 19:43:10 -0700

    First commit

---
+++ rest.go
@@ -0,0 +1,51 @@
+// this runs our net server
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// data structures putting in the json
+type Payload struct {
+	Stuff Data
+}
+
+type Data struct {
+	Fruit   Fruits
+	Veggies Vegetables
+}
+
+type Fruits map[string]int
+type Vegetables map[string]int
+
+func serveRest(w http.ResponseWriter, r *http.Request) {
+	// writes to the response writer - we need to need json for it
+	response, err := getJsonResponse()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprint(w, string(response))
+}
+
+func main() {
+	http.HandleFunc("/", serveRest)
+	http.ListenAndServe("localhost:1337", nil)
+
+}
+
+func getJsonResponse() ([]byte, error) {
+	Fruits := make(map[string]int)
+	Fruits["Apples"] = 25
+	Fruits["Oranges"] = 11
+	Vegetables := make(map[string]int)
+	Vegetables["Carrots"] = 21
+	Vegetables["Peppers"] = 0
+
+	d := Data{Fruits, Vegetables}
+	p := Payload{d}
+
+	return json.MarshalIndent(p, "", "  ")
+}
